40f9b683dcff5ac6dc568d4cc9d9cdf1b4047c0f
jhordanres <jhordanres@hotmail.com>
2022-09-22 17:44:36 -0500

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,10 +1,23 @@
 const {io} = require('../index.js');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Guns and Rose'));
+bands.addBand(new Band('Mago de Oz'));
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Mana'));
+console.log(bands);
 
 
 
 //Mensajes de Sockets y conectar
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    //Para emitirles todas las bandas al cliente que se conecte
+    client.emit('active-bands', bands.getBands());
 
     //Para desconectar al usuario del server
     client.on('disconnect', () => {
@@ -19,5 +32,26 @@ io.on('connection', client => {
 
     });
 
-    
+    client.on('emitir-mensaje', (payload) =>{
+        //io.emit('nuevo-mensaje', payload); //Esto emite el mensaje a todos
+        client.broadcast.emit('nuevo-mensaje', payload); //Emite el mensaje a todos menos al que lo emitio
+        //console.log(payload);
+    });
+
+    client.on('vote-band', (payload) =>{
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) =>{
+        const newBand = new Band(payload.name)
+        bands.addBand(newBand)
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delet-band', (payload) =>{
+        bands.deletBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
   });
